1c7051a6899d93f6c5f4ff3682b0e5cf14a50bcd
Simsblock <146435639+Simsblock@users.noreply.github.com>
2025-12-03 00:10:28 +0100

    MossMask Charm Behaviour

--- src/Charms/MossMask.cs
+++ src/Charms/MossMask.cs
@@ -1,91 +1,38 @@
-﻿using System.Collections;
-using DistantGreensCharms.Helper;
-using DistantGreensCharms.HUDElements;
-using DistantGreensCharms.Settings;
-using IL.HutongGames.PlayMaker.Actions;
-using ItemChanger.Items;
+﻿using ItemChanger.Items;
 using Modding;
-using UnityEngine;
-using UnityEngine.SceneManagement;
 
 namespace DistantGreensCharms.Charms;
 
 public class MossMask : ACharm
 {
     public static readonly MossMask Instance = new();
-    private bool _charged = true;
-    private bool Useable => _charged && Equipped();
     
-    public override string SpritePath  => "CharmIcons.MossMask"; 
+    public override string Sprite  => "CharmIcons/MossMask.png"; //test
     public override string Name => "Moss-Mask"; 
-    public override string Description => "Appears like a Mask protecting its wearer.\nShields against critical damage,but shatters on impact.\n\nIf shattered, the mask will repair itself while resting at a bench.";
+    public override string Description => "Moss-Mask description"; //todo
     public override int DefaultCost => 3;
-    public override string SceneName => "GG_Workshop"; //todo 
-    public override float X => 17.65f; //todo 
-    public override float Y => 6.41f; //todo 
-    
-    public override CharmState State(LocalSettings s) => s.MossMask;
-
-    private MossMaskHUD HUD => MossMaskHUD.Instance;
+    public override string Scene => "GG_Workshop"; //todo
+    public override float X => 17.65f; //todo
+    public override float Y => 6.41f; //todo
 
     public override void Hook()
     {
-        UnityEngine.SceneManagement.SceneManager.activeSceneChanged += OnSceneChange;
         ModHooks.TakeHealthHook += CheckMaskActivation;
-        ModHooks.SetPlayerBoolHook += OnSetPlayerBool;
-        On.GameManager.EquipCharm += OnEquipCharm;
-        On.GameManager.UnequipCharm += OnUnequipCharm;
+        ModHooks.SetPlayerBoolHook += ChargeCharm;
     }
-    
-    private void OnSceneChange(Scene from, Scene to)
-    {
-        if (!Equipped()) return;
-        if (to.name.Contains("Dream") || to.name.Contains("GG_Atrium") || to.name.Equals("GG_Workshop")) SetCharged(true);
-    }
+
+    private bool charged = true;
     private int CheckMaskActivation(int damage)
     {
-        if (!Useable) return damage;
-        bool JonisBlessing = PlayerData.instance.equippedCharm_27;
-        if (JonisBlessing && PlayerData.instance.healthBlue+1 - damage <= 0) // Jonis gives Player 12 blueHealth BUT keeps 1 health -> +1
-        {
-            PlayerData.instance.health = 1;
-            PlayerData.instance.healthBlue = 0;
-            SetCharged(false);
-            return 0;
-        }
-        if (!JonisBlessing && PlayerData.instance.health - damage <= 0)
-        {
-            PlayerData.instance.health = 1; 
-            SetCharged(false); 
-            return 0;
-        }
-        return damage;
+        if (!Equipped() || !charged || PlayerData.instance.health - damage > 0) return damage;
+        if (PlayerData.instance.health - damage > 0) return damage;
+        charged = false;
+        return 0;
     }
 
-    private bool OnSetPlayerBool(string target, bool value)
+    private bool ChargeCharm(string targetBool, bool value)
     {
-        if (target == "atBench" && value && Equipped())
-        {
-            SetCharged(true);
-        }
+        if(targetBool == "atBench" && value && Equipped()) charged = true;
         return value;
-    }
-    
-    private void OnEquipCharm(On.GameManager.orig_EquipCharm orig, GameManager self, int charmnum)
-    {
-        orig(self, charmnum);
-        if(charmnum == Num) HUD.SetVisibility(true);
-    }
-    
-    private void OnUnequipCharm(On.GameManager.orig_UnequipCharm orig, GameManager self, int charmnum)
-    {
-        orig(self, charmnum);
-        if(charmnum == Num) HUD.SetVisibility(false);
-    }
-
-    private void SetCharged(bool charged)
-    {
-        _charged = charged;
-        HUD.UpdateSpriteState(charged);
     }
 }
